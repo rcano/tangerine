2679eba6e6a12c2afbc79771d4f6760ad3598ec7
rcano <ioniviil@gmail.com>
2018-12-13 21:25:11 -0300

    support for mapping observable lists

--- src/main/scala/tangerine/package.scala
+++ src/main/scala/tangerine/package.scala
@@ -2,6 +2,9 @@ package tangerine
 
 import javafx.beans.value.ObservableValue
 import javafx.scene.{Node, Parent}
+import javafx.collections.ListChangeListener
+import javafx.collections.ObservableList
+import javafx.collections.transformation.TransformationList
 import javafx.scene.control._
 import javafx.scene.text.Font
 import scala.collection.JavaConverters._
@@ -24,6 +27,49 @@ object `package` {
     @inline def tap(f: T => Any): T = {
       f(self)
       self
+    }
+  }
+  
+  implicit class ObservableListExt[T](private val list: ObservableList[T]) extends AnyVal {
+    /**
+     * Creates an observable list which is equivalent to the passed list transformed with the function `f` and kept in sync regarding changes.
+     * Notice that the f function is applied lazily (this implies non eagerness and memoization)
+     */
+    def mapLazy[U](f: T => U): ObservableList[U] = new TransformationList[U, T](list) {
+      private var cache = scala.collection.immutable.IntMap.empty[U]
+      private val reverseIndex = scala.collection.mutable.HashMap.empty[T, Int]
+      override def get(i: Int) = {
+        cache.get(i) match {
+          case Some(e) => e
+          case _ => 
+            val t = list.get(i)
+            reverseIndex(t) = i
+            f(t) tap (u => cache = cache.updated(i, u))
+        }
+      }
+      override def size() = list.size
+      override def getSourceIndex(i: Int) = i
+      override def getViewIndex(i: Int) = i
+      override protected def sourceChanged(evt: ListChangeListener.Change[_ <: T]) = {
+        while (evt.next) {
+          if (evt.wasPermutated || evt.wasUpdated) { for (i <- evt.getFrom until evt.getTo) cache -= i }
+          if (evt.wasRemoved) evt.getRemoved.forEach { e => reverseIndex.remove(e) foreach (cache -= _) }
+        }
+      }
+    }
+    /**
+     * Creates an observable list which is equivalent to the passed list transformed with the function `f` and kept in sync regarding changes.
+     * Notice that the `f` function is applied every time an element is requested, because the transformation is not stored. For a behaviour
+     * that does store see [[mapLazy]]
+     */
+    def mapNoCache[U](f: T => U): ObservableList[U] = new TransformationList[U, T](list) {
+      override def get(i: Int) = list.get(i) pipe f
+      override def size() = list.size
+      override def getSourceIndex(i: Int) = i
+      override def getViewIndex(i: Int) = i
+      override protected def sourceChanged(evt: ListChangeListener.Change[_ <: T]) = {
+        
+      }
     }
   }
   
